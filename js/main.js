031f14976b99203a8b3ab2a13cae97f2b1ccc5d2
Mateus Santana <mateusmsant@gmail.com>
2021-02-13 04:10:46 -0300

    primeiro commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:5,
+  nav:false,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  },
+  
+})
+
+jQuery(document).ready(function() {
+    jQuery('#loading').fadeOut(3000);
+});
